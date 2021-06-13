378c616e4f73d5c99a66622d6591ffd7d7e521b7
AJ Auntor <auntorknocks@gmail.com>
2021-06-13 15:06:03 +0600

    storage east

--- program_one.py
+++ program_one.py
@@ -106,3 +106,15 @@ class Dog:
 ['roll over']
 >>> e.tricks
 ['play dead']
+
+>>> class Warehouse:
+        purpose = 'storage'
+        region = 'west'
+
+>>> w1 = Warehouse()
+>>> print(w1.purpose, w1.region)
+storage west
+>>> w2 = Warehouse()
+>>> w2.region = 'east'
+>>> print(w2.purpose, w2.region)
+# storage east
