b8171f57f2e15686a951fa7c39ad6e8f3913bec6
AJ Auntor <auntorknocks@gmail.com>
2021-06-13 15:05:08 +0600

    play dead

--- program_one.py
+++ program_one.py
@@ -71,3 +71,20 @@ class Dog:
 'Fido'
 >>> e.name                  # unique to e
 'Buddy'
+
+class Dog:
+
+    tricks = []             # mistaken use of a class variable
+
+    def __init__(self, name):
+        self.name = name
+
+    def add_trick(self, trick):
+        self.tricks.append(trick)
+
+>>> d = Dog('Fido')
+>>> e = Dog('Buddy')
+>>> d.add_trick('roll over')
+>>> e.add_trick('play dead')
+>>> d.tricks                # unexpectedly shared by all dogs
+['roll over', 'play dead']
