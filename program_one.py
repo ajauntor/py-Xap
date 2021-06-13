1cf14ea0f3253eda0d5e39a9e0b2e809d353e4c8
AJ Auntor <auntorknocks@gmail.com>
2021-06-13 15:03:17 +0600

    return program

--- program_one.py
+++ program_one.py
@@ -33,6 +33,15 @@ class MyClass:
     def f(self):
         return 'hello world'
 
+        >>> class Complex:
+...     def __init__(self, realpart, imagpart):
+...         self.r = realpart
+...         self.i = imagpart
+...
+>>> x = Complex(3.0, -4.5)
+>>> x.r, x.i
+(3.0, -4.5)
+
 # #python string
 # print("Auntor");
 
