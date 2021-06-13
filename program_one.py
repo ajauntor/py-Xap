7785c9949da0a00c1b7eac30b2bf1021b063ec02
AJ Auntor <auntorknocks@gmail.com>
2021-06-13 15:05:36 +0600

    rool over

--- program_one.py
+++ program_one.py
@@ -88,3 +88,21 @@ class Dog:
 >>> e.add_trick('play dead')
 >>> d.tricks                # unexpectedly shared by all dogs
 ['roll over', 'play dead']
+
+class Dog:
+
+    def __init__(self, name):
+        self.name = name
+        self.tricks = []    # creates a new empty list for each dog
+
+    def add_trick(self, trick):
+        self.tricks.append(trick)
+
+>>> d = Dog('Fido')
+>>> e = Dog('Buddy')
+>>> d.add_trick('roll over')
+>>> e.add_trick('play dead')
+>>> d.tricks
+['roll over']
+>>> e.tricks
+['play dead']
