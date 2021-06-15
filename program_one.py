66990799453b6f8641baaac0cd73d0881841483c
AJ Auntor <auntorknocks@gmail.com>
2021-06-15 15:14:22 +0600

    create empty amployee record

--- program_one.py
+++ program_one.py
@@ -142,21 +142,31 @@ class C:
         self.add(x)
         self.add(x)
 
-        class Mapping:
-    def __init__(self, iterable):
-        self.items_list = []
-        self.__update(iterable)
+#         class Mapping:
+#     def __init__(self, iterable):
+#         self.items_list = []
+#         self.__update(iterable)
 
-    def update(self, iterable):
-        for item in iterable:
-            self.items_list.append(item)
+#     def update(self, iterable):
+#         for item in iterable:
+#             self.items_list.append(item)
 
-    __update = update   # private copy of original update() method
+#     __update = update   # private copy of original update() method
 
-class MappingSubclass(Mapping):
+# class MappingSubclass(Mapping):
 
-    def update(self, keys, values):
-        # provides new signature for update()
-        # but does not break __init__()
-        for item in zip(keys, values):
-            self.items_list.append(item)
+#     def update(self, keys, values):
+#         # provides new signature for update()
+#         # but does not break __init__()
+#         for item in zip(keys, values):
+#             self.items_list.append(item)
+
+#     class Employee:
+#     pass
+
+john = Employee()  # Create an empty employee record
+
+# Fill the fields of the record
+john.name = 'John Doe'
+john.dept = 'computer lab'
+john.salary = 1000
