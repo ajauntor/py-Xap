41a8dddf8101b2bc46aec4e8a70273cccafd09c9
AJ Auntor <auntorknocks@gmail.com>
2021-06-15 15:16:36 +0600

    sum i*i

--- program_one.py
+++ program_one.py
@@ -197,3 +197,19 @@ for line in open("myfile.txt"):
             raise StopIteration
         self.index = self.index - 1
         return self.data[self.index]
+
+#         >>> sum(i*i for i in range(10))                 # sum of squares
+# 285
+
+# >>> xvec = [10, 20, 30]
+# >>> yvec = [7, 5, 3]
+# >>> sum(x*y for x,y in zip(xvec, yvec))         # dot product
+# 260
+
+# >>> unique_words = set(word for line in page  for word in line.split())
+
+# >>> valedictorian = max((student.gpa, student.name) for student in graduates)
+
+# >>> data = 'golf'
+# >>> list(data[i] for i in range(len(data)-1, -1, -1))
+# ['f', 'l', 'o', 'g']
