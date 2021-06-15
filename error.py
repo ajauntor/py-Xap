692e72066d2b1f1a152370fb94819a19330c7880
AJ Auntor <auntorknocks@gmail.com>
2021-06-15 15:19:59 +0600

    runtime

--- error.py
+++ error.py
@@ -19,3 +19,19 @@ dividend = float(input("Please enter the dividend: "))
 divisor = float(input("Please enter the divisor: "))
 quotient = dividend / divisor
 quotient_rounded = math.round(quotient)
+
+for x in range(a, b):
+    print("(%f, %f, %f)" % my_list[x])
+
+product = 0
+for i in range(10):
+    product *= i
+
+sum_squares = 0
+for i in range(10):
+    i_sq = i**2
+sum_squares += i_sq
+
+nums = 0
+for num in range(10):
+    num += num
