721d9010152209ffeeba0c0e2af7728a31804275
AJ Auntor <auntorknocks@gmail.com>
2021-06-15 15:15:42 +0600

    class reverse

--- program_one.py
+++ program_one.py
@@ -182,3 +182,18 @@ for char in "123":
     print(char)
 for line in open("myfile.txt"):
     print(line, end='')
+
+    class Reverse:
+    """Iterator for looping over a sequence backwards."""
+    def __init__(self, data):
+        self.data = data
+        self.index = len(data)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.index == 0:
+            raise StopIteration
+        self.index = self.index - 1
+        return self.data[self.index]
