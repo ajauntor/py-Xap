cb72563a15a7bfc0cc1b1cece11d7cd5e81cdea4
AJ Auntor <auntorknocks@gmail.com>
2021-06-15 15:18:31 +0600

    syntext error

---
+++ error.py
@@ -0,0 +1,16 @@
+myfunction(x, y):
+    return x + y
+
+else:
+    print("Hello!")
+
+if mark >= 50
+    print("You passed!")
+
+if arriving:
+    print("Hi!")
+esle:
+    print("Bye!")
+
+if flag:
+print("Flag is set!")
