0614a2ba4c28f1907d60a0000b3edd1fe7759513
AJ Auntor <auntorknocks@gmail.com>
2021-06-16 15:27:00 +0600

    try

--- except.py
+++ except.py
@@ -7,9 +7,7 @@ except(ValueError, ZeroDivisionError):
 
 
 try:
-    dividend = int(input("Please enter the dividend: "))
-    divisor = int(input("Please enter the divisor: "))
-    print("%d / %d = %f" % (dividend, divisor, dividend/divisor))
+    
 except ValueError:
     print("The divisor and dividend have to be numbers!")
 except ZeroDivisionError:
