31c6af1471456648011df4cf46ca76045eca19dd
AJ Auntor <auntorknocks@gmail.com>
2021-06-16 15:24:32 +0600

    ValueError

--- except.py
+++ except.py
@@ -14,3 +14,19 @@ except ValueError:
     print("The divisor and dividend have to be numbers!")
 except ZeroDivisionError:
     print("The dividend may not be zero!")
+
+
+try:
+    dividend = int(input("Please enter the dividend: "))
+except ValueError:
+    print("The dividend has to be a number!")
+
+try:
+    divisor = int(input("Please enter the divisor: "))
+except ValueError:
+    print("The divisor has to be a number!")
+
+try:
+    print("%d / %d = %f" % (dividend, divisor, dividend/divisor))
+except ZeroDivisionError:
+    print("The dividend may not be zero!")
