6efe4849374a4eaa546ad9fb7692049397facdee
AJ Auntor <auntorknocks@gmail.com>
2021-06-16 15:23:54 +0600

    except apply

--- except.py
+++ except.py
@@ -4,3 +4,13 @@ try:
     print("%d / %d = %f" % (dividend, divisor, dividend/divisor))
 except(ValueError, ZeroDivisionError):
     print("Oops, something went wrong!")
+
+
+try:
+    dividend = int(input("Please enter the dividend: "))
+    divisor = int(input("Please enter the divisor: "))
+    print("%d / %d = %f" % (dividend, divisor, dividend/divisor))
+except ValueError:
+    print("The divisor and dividend have to be numbers!")
+except ZeroDivisionError:
+    print("The dividend may not be zero!")
