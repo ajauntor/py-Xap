18a9030048eaf1e028c4a15e44226c2db36ea999
AJ Auntor <auntorknocks@gmail.com>
2021-07-25 11:28:25 +0600

    variable dicear

---
+++ variable-and-dataType.py
@@ -0,0 +1,6 @@
+name = "Novana"
+
+print("This student name is" + " " +name)
+print( name+ " " + "is currently doing study in class ten")
+print("This year boy will going to be age 23 age year old.")
+print("Ok, Now we can go for code now")
